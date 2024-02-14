668e08cac79de71d78a66b2a7a64af079a4a5556
Hao Ye <lhopitalified@gmail.com>
2024-02-14 10:30:48 -0500

    add checking logic to update script

--- update-and-rebuild.sh
+++ update-and-rebuild.sh
@@ -1,4 +1,25 @@
-cd /home/haoye/projects/roadmap-automation/
-git pull
+dir="/home/haoye/projects/roadmap-automation/"
 
-/usr/bin/podman -t roadmap-automation .
+# navigate to correct director
+cd $dir
+
+# check for new commits
+origin="origin"
+branch="main"
+commit=$(git log -n 1 --pretty=format:%H "$origin/$branch")
+url=$(git remote get-url "$origin")
+has_updates=false
+
+for line in "$(git ls-remote -h $url)"; do
+    fields=($(echo $line | tr -s ' ' ))
+    test "${fields[1]}" == "refs/heads/$branch" || continue
+    if "${fields[0]}" != "$commit" ; then
+        has_updates=true
+        break
+    fi
+done
+
+if $has_updates ; then
+    git pull
+    /usr/bin/podman -t roadmap-automation .
+fi
