794c67621774cceb53a8bc72345eb880c3de3093
Hao Ye <lhopitalified@gmail.com>
2024-02-14 10:36:23 -0500

    fix script

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -1,6 +1,6 @@
 dir="/home/haoye/projects/roadmap-automation/"
 
-# navigate to correct director
+# navigate to correct directory
 cd $dir
 
 # check for new commits
@@ -13,12 +13,13 @@ has_updates=false
 for line in "$(git ls-remote -h $url)"; do
     fields=($(echo $line | tr -s ' ' ))
     test "${fields[1]}" == "refs/heads/$branch" || continue
-    if "${fields[0]}" != "$commit" ; then
+    if [ "${fields[0]}" != "$commit" ] ; then
         has_updates=true
         break
     fi
 done
 
+# update and rebuild
 if $has_updates ; then
     git pull
     /usr/bin/podman -t roadmap-automation .
