c58fcf468173b62923b9a592105cb4abd6bc6de1
Hao Ye <lhopitalified@gmail.com>
2024-02-14 10:03:32 -0500

    add scripts to build and run

---
+++ run-sync.sh
@@ -0,0 +1,1 @@
+/usr/bin/podman run -v /home/haoye/projects/roadmap-automation/.Renviron:/home/c4r-automation/.Renviron roadmap-automation:latest
---
+++ update-and-rebuild.sh
@@ -0,0 +1,4 @@
+cd /home/haoye/projects/roadmap-automation/
+git pull
+
+/usr/bin/podman -t roadmap-automation .
